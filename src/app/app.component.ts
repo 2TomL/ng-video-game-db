9d8adba53020bc853a1243f2dac1d0176858be73
2TomL <lamerstom.123@gmail.com>
2023-10-03 09:28:45 +0200

    push 4 nothing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './services/http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  constructor(private httpService: HttpService) {}
+  ngOnInit(): void {
+    this.httpService
+      .getGameList('some_ordering', 'search_query')
+      .subscribe((data) => {
+        // Handle the API response here
+      });
+
+    this.httpService.getGameDetails('game_id').subscribe((data) => {
+      // Handle the API response here
+    });
+  }
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { forkJoin, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -10,6 +10,9 @@ import { APIResponse, Game } from '../models';
 })
 export class HttpService {
 
+  private rapidApiKey = 'edbb1b1bb3mshd01e55b2b432362p170af0jsnb241407192a4';
+  private rapidApiHost = 'rawg-video-games-database.p.rapidapi.com';
+
   constructor(private http: HttpClient) { }
 
   getGameList(
@@ -19,22 +22,29 @@ export class HttpService {
     let params = new HttpParams().set('ordering', ordering);
 
     if (search) {
-      params = new HttpParams().set('ordering', ordering).set('search', search);
+      params = params.set('search', search);
     }
+
+    const headers = new HttpHeaders({
+      'X-RapidAPI-Key': this.rapidApiKey,
+      'X-RapidAPI-Host': this.rapidApiHost
+    });
 
     return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
       params: params,
+      headers: headers
     });
   }
 
   getGameDetails(id: string): Observable<Game> {
-    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`);
-    const gameTrailersRequest = this.http.get(
-      `${env.BASE_URL}/games/${id}/movies`
-    );
-    const gameScreenshotsRequest = this.http.get(
-      `${env.BASE_URL}/games/${id}/screenshots`
-    );
+    const headers = new HttpHeaders({
+      'X-RapidAPI-Key': this.rapidApiKey,
+      'X-RapidAPI-Host': this.rapidApiHost
+    });
+
+    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`, { headers });
+    const gameTrailersRequest = this.http.get(`${env.BASE_URL}/games/${id}/movies`, { headers });
+    const gameScreenshotsRequest = this.http.get(`${env.BASE_URL}/games/${id}/screenshots`, { headers });
 
     return forkJoin({
       gameInfoRequest,
